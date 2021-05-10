28a732212d1bce40199b59945b8177ef4dbabeb1
Vasyl <vasyl.zmiy@gmail.com>
2021-05-10 20:45:37 +0300

    q

--- views/views.py
+++ views/views.py
@@ -12,9 +12,24 @@ def homepage():
 
 @app.route('/departments')
 def departments():
+
     res = Department.query.all()
+    salary_list = []
+    for dep in res:
+        sum_salary = 0
+        counter = 0
+        list_of_employees = Employee.query.filter(Employee.department_id == dep.id)
+        print('--------------')
+        for i in list_of_employees:
+            sum_salary += i.salary
+            counter += 1
+        if counter != 0:
+            salary_list.append(round(sum_salary/counter, 2))
+        else:
+            salary_list.append(0)
     print(res)
-    return render_template('departments.html', res=res)
+    result = zip(res, salary_list)
+    return render_template('departments.html', result=result)
 
 
 @app.route('/add_dep', methods=['POST', 'GET'])
@@ -28,11 +43,20 @@ def add_dep():
     return render_template('add_dep.html')
 
 
+@app.route('/edit_dep', methods=['POST', 'GET'])
+def edit_dep():
+    if request.method == 'POST':
+        print(request.form)
+        dep = Department(name=request.form.get('first', False))
+        db.session.add(dep)
+        db.session.commit()
+
+    return render_template('edit_dep.html')
+
+
 @app.route('/department/<dep_id>')
 def department(dep_id):
     res = Employee.query.filter(Employee.department_id == dep_id)
-    for i in res:
-        print(i)
     return render_template('department.html', dep_id=dep_id, res=res)
 
 
@@ -41,3 +65,11 @@ def profile(user_id):
     res = Employee.query.filter(Employee.id == user_id)
     print(res)
     return render_template('profile.html', res=res[0])
+
+
+@app.route('/del_dep/<dep_id>', methods=['POST', 'GET'])
+def del_dep(dep_id):
+    res = Department.query.filter(Department.id == dep_id)
+    db.session.delete(res[0])
+    db.session.commit()
+    return redirect(url_for('departments', dep_id=dep_id))
